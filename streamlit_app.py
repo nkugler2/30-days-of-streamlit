326845f632ed11e3954fb5383ed646762e4a7481
Noah <nkugler2@asu.edu>
2026-04-04 03:08:14 -0700

    Woops, did not include the actual change to the streamlit_app for day 5

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,35 @@
 import streamlit as st
+import numpy as np
+import altair as alt
+import pandas as pd
 
-st.header("30-days-of-streamlit")
-st.caption("Can Noah write code without AI (kind of)")
+st.header('st.write')
 
-st.header("st.button")
+# Example 1
 
-if st.button('Say Hello'):
-    st.write('Why hello there')
-else:
-    st.write('Goodbye')
+st.write('Hello, *World!* :sunglasses:')
+
+# Example 2
+
+st.write(1234)
+
+# Example 3
+
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
+
+# Example 4
+
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+# Example 5
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
